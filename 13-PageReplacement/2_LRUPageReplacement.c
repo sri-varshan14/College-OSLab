3759b16526c9de3a6fb3076447dd18e4796982c8
sri-varshan14 <mailmeatsri14@gmail.com>
2022-06-06 23:48:50 +0530

    fixing code

--- 13-PageReplacement/2_LRUPageReplacement.c
+++ 13-PageReplacement/2_LRUPageReplacement.c
@@ -1,61 +1,91 @@
-// *** Program in Error ***
 #include <stdio.h>
-int arrmin(int[], int);
-main(){
-    int i,j,len,rs[50],frame[10],nf,k,avail,count=0;
-    int access[10], freq=0, dm;
-    printf("Length of Reference string : ");
-    scanf("%d", &len);
-    printf("Enter reference string :\n");
-    for(i=1; i<=len; i++)
-        scanf("%d", &rs[i]);
-    printf("Enter no. of frames : ");
-    scanf("%d", &nf);
-    for(i=0; i<nf; i++)
-        frame[i] = -1;
-    j = 0;
-    printf("\nRef. str Page frames");
-    for(i=1; i<=len; i++){
-        printf("\n%4d\t", rs[i]);
-        avail = 0;
-        for(k=0; k<nf; k++){
-            if(frame[k] == rs[i]){
-                avail = 1;
-                access[k] = ++freq;
-                break;
-            }
-        }
-        if(avail == 0){
-            dm = 0;
-            for(k=0; k<nf; k++){
-                if(frame[k] == -1)
-                    dm = 1;
-                break;
-            }
-            if(dm == 1){
-                frame[k] = rs[i];
-                access[k] = ++freq;
-                count++;
-            } 
-            else{
-                j = arrmin(access, nf);
-                frame[j] = rs[i];
-                access[j] = ++freq;
-                count++;
-            }
-            for(k=0; k<nf; k++)
-                printf("%4d", frame[k]);
-        }
-    }
-    printf("\n\nTotal no. of page faults : %d\n", count);
+ 
+int findLRU(int time[], int n)
+{
+   int i, minimum = time[0], pos = 0;
+ 
+   for (i = 1; i < n; ++i)
+   {
+      if (time[i] < minimum)
+      {
+         minimum = time[i];
+         pos = i;
+      }
+   }
+ 
+   return pos;
 }
-
-int arrmin(int a[], int n){
-    int i, min = a[0];
-    for(i=1; i<n; i++)
-        if (min > a[i])
-            min = a[i];
-    for(i=0; i<n; i++)
-        if (min == a[i])
-            return i;
+ 
+int main()
+{
+   int no_of_frames, no_of_pages, frames[10], pages[30], counter = 0, time[10], flag1, flag2, i, j, pos, faults = 0;
+   printf("Enter number of frames: ");
+   scanf("%d", &no_of_frames);
+ 
+   printf("Enter number of pages: ");
+   scanf("%d", &no_of_pages);
+ 
+   printf("Enter reference string: ");
+ 
+   for (i = 0; i < no_of_pages; ++i)
+   {
+      scanf("%d", &pages[i]);
+   }
+ 
+   for (i = 0; i < no_of_frames; ++i)
+   {
+      frames[i] = -1;
+   }
+ 
+   for (i = 0; i < no_of_pages; ++i)
+   {
+      flag1 = flag2 = 0;
+ 
+      for (j = 0; j < no_of_frames; ++j)
+      {
+         if (frames[j] == pages[i])
+         {
+            counter++;
+            time[j] = counter;
+            flag1 = flag2 = 1;
+            break;
+         }
+      }
+ 
+      if (flag1 == 0)
+      {
+         for (j = 0; j < no_of_frames; ++j)
+         {
+            if (frames[j] == -1)
+            {
+               counter++;
+               faults++;
+               frames[j] = pages[i];
+               time[j] = counter;
+               flag2 = 1;
+               break;
+            }
+         }
+      }
+ 
+      if (flag2 == 0)
+      {
+         pos = findLRU(time, no_of_frames);
+         counter++;
+         faults++;
+         frames[pos] = pages[i];
+         time[pos] = counter;
+      }
+ 
+      printf("\n");
+ 
+      for (j = 0; j < no_of_frames; ++j)
+      {
+         printf("%d\t", frames[j]);
+      }
+   }
+ 
+   printf("\nTotal Page Faults = %d", faults);
+ 
+   return 0;
 }
--- 14-Directory/2_TwoDirectory.c
+++ 14-Directory/2_TwoDirectory.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include<string.h>
 struct{
     char dname[10], fname[10][10];
     int fcnt;
